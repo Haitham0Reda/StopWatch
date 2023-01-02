25aa0332675f3134a5db8354a61eef983c545ffe
Haitham0Reda <102927584+Haitham0Reda@users.noreply.github.com>
2023-01-02 22:24:32 +0200

    Update JS

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 
+let minutes = 00;
+let seconds = 00;
+let tens = 00;
+
+let getMinutes = document.querySelector('.minutes');
+let getSeconds = document.querySelector('.seconds');
+let getTens = document.querySelector('.tens');
+let btnStart = document.querySelector('.btn-start');
+let btnStop = document.querySelector('.btn-stop');
+let btnReset = document.querySelector('.btn-reset');
+
+btnStart.addEventListener('click', () => {
+    setInterval(startTimer, 10)
+})
+
+function startTimer() {
+    tens++;
+    if (tens <= 9) {
+        getTens.innerHTML = '0' + tens;
+    }
+    if (tens > 9) {
+        getTens.innerHTML = tens;
+    }
+    if (tens > 99) {
+        seconds++;
+        getSeconds.innerHTML = '0' + seconds;
+        tens = 0;
+        getTens.innerHTML = '0' + 0;
+    }
+}
